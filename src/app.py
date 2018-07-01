fe2259e82e6344502b729478c5a752f75139013e
neogeoss <neogeo-s@hanmail.net>
2018-07-02 00:59:50 +0900

    base html and jinja2 used to extend it

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 __author__ = 'inchan'
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, world!"
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(port=4995)
