ec8a219d5521f8271dbb1def1926b72cd9ac1714
neogeoss <neogeo-s@hanmail.net>
2018-07-01 15:55:05 +0900

    User model made

---
+++ src/models/user.py
@@ -0,0 +1,65 @@
+import uuid
+from src.common.database import Database
+
+class User():
+    def __init__(self, email, password, _id=None):
+        self.email = email
+        self.passwprd = password
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+
+    @classmethod
+    def get_by_email(self, email):
+        data = Database.find_one("users", {"email": email})
+
+        if data is not None:
+            return cls(**data)
+
+
+
+    @classmethod
+    def get_by_id(self, _id):
+        data = Database.find_one("users", {"_id": _id})
+
+        if data is not None:
+            return cls(**data)
+
+    @staticmethod
+    def login_valid(self, email, password):
+        user = User.get_by_email(email)
+        if user is not None:
+            return user.password == password
+        return False
+
+    @classmethod
+    def register(email, password):
+        user = User.get_by_email(email)
+        if user is not None:
+            new_user = cls(email, password)
+            new_user.save_to_mongo()
+            session['email'] = email
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def login(user_email):
+        session['email'] = user_email
+
+    def logout():
+        session['email'] = None
+
+    def get_blogs(self):
+        return Blog.find_by_author_id(self._id)
+
+    def json(self):
+        return {
+            "email" : self.email,
+            "_id" : self._id,
+            "password" : self.password
+        }
+
+
+
+    def save_to_mongo(self):
+        Database.insert("users", self.json())
