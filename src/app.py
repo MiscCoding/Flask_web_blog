3d27ae2ecf455cbda524e105fc0d15e5a3d0539b
neogeoss <neogeo-s@hanmail.net>
2018-07-05 20:34:54 +0900

    Bug fixed and new post page added

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from src.common.database import Database
 from src.models.blog import Blog
+from src.models.post import Post
 from src.models.user import User
 
 __author__ = 'inchan'
@@ -67,7 +68,7 @@ def blog_posts(blog_id):
     blog = Blog.from_mongo(blog_id)
     posts = blog.get_posts()
 
-    return render_template('posts.html', posts=posts, blog_title = blog.title)
+    return render_template('posts.html', posts=posts, blog_title = blog.title, blog_id = blog_id)
 
 @app.route('/blogs/new', methods=['POST','GET'])
 def create_new_blog():
@@ -82,6 +83,21 @@ def create_new_blog():
         new_blog.save_to_mongo()
 
         return make_response(user_blogs(user._id))
+
+
+@app.route('/posts/new/<string:blog_id>', methods=['POST', 'GET'])
+def create_new_post(blog_id):
+    if request.method == 'GET':
+        return render_template('new_post.html', blog_id = blog_id)
+    else:
+        title = request.form['title']
+        content = request.form['content']
+        user = User.get_by_email(session['email'])
+
+        new_post= Post(blog_id, title, content, user.email)
+        new_post.save_to_mongo()
+
+        return make_response(blog_posts(blog_id))
 
 if __name__ == '__main__':
     app.config.update(DEBUG=True)
